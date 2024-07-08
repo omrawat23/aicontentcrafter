88e7aa2e5005a0df26e3aa8272f73d2190359188
omrawat23 <omrawat23@gmail.com>
2024-07-08 13:43:38 +0530

    deployment:fixed

--- app/dashboard/content/_components/FormSection.tsx
+++ app/dashboard/content/_components/FormSection.tsx
@@ -29,38 +29,39 @@ function FormSection({ selectedTemplate,userFormInput,loading }: PROPS) {
 
     return (
         <div className='p-5 shadow-md border rounded-lg bg-white'>
-            {/* @ts-ignore */}
-            <Image src={selectedTemplate?.icon}
-                alt='icon' width={70} height={70} />
-            <h2 className='font-bold text-2xl mb-2 mt-4 text-primary'>{selectedTemplate?.name}</h2>
-            <p className='text-gray-500 text-sm'>{selectedTemplate?.desc}</p>
+            {selectedTemplate && (
+                <>
+                    <Image src={selectedTemplate.icon}
+                        alt='icon' width={70} height={70} />
+                    <h2 className='font-bold text-2xl mb-2 mt-4 text-primary'>{selectedTemplate.name}</h2>
+                    <p className='text-gray-500 text-sm'>{selectedTemplate.desc}</p>
+                </>
+            )}
 
             <form className='mt-6' onSubmit={onSubmit}>
                 {selectedTemplate?.form?.map((item, index) => (
-                    <div className='my-2 flex flex-col gap-2 mb-7'>
+                    <div key={item.name} className='my-2 flex flex-col gap-2 mb-7'>
                         <label className='font-bold'>{item.label}</label>
-                        {item.field == 'input' ?
-                            <Input name={item.name} required={item?.required}
-                            onChange={handleInputChange}
-                            />
-                            : item.field == 'textarea' ?
+                        {item.field === 'input' ? (
+                            <Input name={item.name} required={item?.required} onChange={handleInputChange} />
+                        ) : item.field === 'textarea' ? (
                             <>
-                                <Textarea name={item.name} required={item?.required}
-                                rows={5}
-                                maxLength={2000}
-                                onChange={handleInputChange} /> 
-                                <label className='text-xs text-gray-400'>Note:Max 2000 Words</label>
-                                
-                                </>    : null
-                        }
+                                <Textarea
+                                    name={item.name}
+                                    required={item?.required}
+                                    rows={5}
+                                    maxLength={2000}
+                                    onChange={handleInputChange}
+                                />
+                                <label className='text-xs text-gray-400'>Note: Max 2000 Words</label>
+                            </>
+                        ) : null}
                     </div>
                 ))}
-                <Button type="submit" 
-                className='w-full py-6'
-                disabled={loading}
-                >
-                    {loading&&<Loader2Icon className='animate-spin'/>}
-                    Generate Content</Button>
+                <Button type="submit" className='w-full py-6' disabled={loading}>
+                    {loading && <Loader2Icon className='animate-spin' />}
+                    Generate Content
+                </Button>
             </form>
         </div>
     )
